3b6186e0ce7a688771b5e6282a267ef07e1403c9
Kei Arruda <marioestevao@gmail.com>
2023-10-27 21:48:22 +0000

    Criação do primeiro modelo e deletando arquivos dummy

---
+++ models/customers.sql
@@ -0,0 +1,19 @@
+with markup as (select * ,
+first_value(customer_id)
+over(partition by company_name, contact_name
+order by company_name
+rows between unbounded preceding and unbounded following) as result
+from {{source('sources', 'customers')}}
+), removed as (
+    select distinct result from markup
+), final as (
+    select * from {{source('sources', 'customers')}} where customer_id in (select result from removed)
+)
+select * from final
+order by company_name
+
+
+
+--select * from markup
+--inner join removed on markup.customer_id = removed.result
+--order by company_name
